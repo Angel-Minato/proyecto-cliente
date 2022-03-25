ab87936d2c587fe99d2f27cf715aaed7e4cf1f68
Angel-Minato <84034853+Angel-Minato@users.noreply.github.com>
2022-03-24 22:13:24 -0600

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 let config = {
-	host: "localhost",
-	user: "root",
-	password: "",
+	host: "us-cdbr-east-05.cleardb.net",
+	user: "/b4278ef1bf7abb",
+	password: "539f654e",
 	port: 3306,
-	database: "adoption_system",
+	database: "heroku_7b6e18b01d8a4eb",
 	secret_key:"napoleon_buena_parte_3D"
 };
 exports.config = config;
