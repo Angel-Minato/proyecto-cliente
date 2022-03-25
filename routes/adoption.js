a20d03700b117500331cd229c5c80fc013411d44
Angel-Minato <84034853+Angel-Minato@users.noreply.github.com>
2022-03-24 23:15:50 -0600

    segundor guardado

--- routes/adoption.js
+++ routes/adoption.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const adoptionsController = require('../controllers/adoptionsController');
-router.get('/adoption', adoptionsController.getAdoption);
+const adoptionValidator = require('../validation/adoptionValidator');
+router.get('/adoption',adoptionValidator.id ,adoptionsController.getAdoption);
 router.get('/adoptions', adoptionsController.getAdoptions);
-router.post('/adoption', adoptionsController.postAdoption);
-router.put('/adoption', adoptionsController.putAdoption);
-router.delete('/adoption', adoptionsController.deleteAdoption);
-router.get('/adoptionUser', adoptionsController.getAdoptionByUser);
+router.post('/adoption',adoptionValidator.add , adoptionsController.postAdoption);
+router.put('/adoption',adoptionValidator.update , adoptionsController.putAdoption);
+router.delete('/adoption',adoptionValidator.id , adoptionsController.deleteAdoption);
+router.get('/adoptionUser',adoptionValidator.userid ,adoptionsController.getAdoptionByUser);
 
 module.exports = router;
