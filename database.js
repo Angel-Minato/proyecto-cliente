3224accb4edf4f030ae44100a81985bdad1bb234
Angel-Minato <84034853+Angel-Minato@users.noreply.github.com>
2022-03-24 22:29:22 -0600

    Update database.js

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 let config = {
 	host: "us-cdbr-east-05.cleardb.net",
-	user: "/b4278ef1bf7abb",
+	user: "b4278ef1bf7abb",
 	password: "539f654e",
 	port: 3306,
 	database: "heroku_7b6e18b01d8a4eb",
